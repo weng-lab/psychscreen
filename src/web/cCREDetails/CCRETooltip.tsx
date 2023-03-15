cb38c03ea7a8c7d406d1dd9060f3d55e07308d8a
hpratt <hpratt111@live.com>
2023-03-15 19:33:06 -0400

    add cCRE tooltip

---
+++ src/web/cCREDetails/CCRETooltip.tsx
@@ -0,0 +1,79 @@
+import React, { useMemo } from 'react';
+import { Loader } from 'semantic-ui-react';
+import { useBiosamples, useCCREInformation } from './hooks';
+
+type CCRETooltipProps = {
+    start: number;
+    end: number;
+    name?: string;
+    assembly?: string;
+};
+
+export const COLORS: Map<string, string> = new Map([
+    [ "PLS", "#ff0000" ],
+    [ "pELS", "#ffa700" ],
+    [ "dELS", "#ffcd00" ],
+    [ "DNase-H3K4me3", "#ffaaaa" ],
+    [ "CTCF-only", "#00b0f0" ]
+]);
+
+export const GROUPS: Map<string, string> = new Map([
+    [ "PLS", "promoter-like" ],
+    [ "pELS", "proximal enhancer-like" ],
+    [ "dELS", "distal enhancer-like" ],
+    [ "DNase-H3K4me3", "DNase-H3K4me3" ],
+    [ "CTCF-only", "CTCF-only" ]
+]);
+
+const MARKS = [ "DNase", "H3K4me3", "H3K27ac", "CTCF" ];
+
+function useBrainBiosamples() {
+    const { data, loading } = useBiosamples();
+    const brainBiosamples = useMemo( () => (
+        data?.ccREBiosampleQuery.biosamples
+            .filter(x => x.ontology.toLocaleLowerCase().includes("brain"))
+    ), [ data ]);
+    return {
+        loading,
+        data: brainBiosamples ? {
+            dnase: new Set(brainBiosamples?.filter(x => x.dnase).map(x => x.dnase!)),
+            h3k4me3: new Set(brainBiosamples?.filter(x => x.h3k4me3).map(x => x.h3k4me3!)),
+            h3k27ac: new Set(brainBiosamples?.filter(x => x.h3k27ac).map(x => x.h3k27ac!)),
+            ctcf: new Set(brainBiosamples?.filter(x => x.ctcf).map(x => x.ctcf!))
+        } : null
+    };
+}
+
+const CCRETooltip: React.FC<CCRETooltipProps> = props => {
+    
+    const { data, loading } = useCCREInformation(props.name || "");
+    const { data: biosamples, loading: biosamplesLoading } = useBrainBiosamples();
+    const maxZ = useMemo( () => data && biosamples && {
+        DNase: Math.max(...[ ...biosamples.dnase ].map(x => data.zScores.get(x) || -10)),
+        H3K4me3: Math.max(...[ ...biosamples.h3k4me3 ].map(x => data.zScores.get(x) || -10)),
+        H3K27ac: Math.max(...[ ...biosamples.h3k27ac ].map(x => data.zScores.get(x) || -10)),
+        CTCF: Math.max(...[ ...biosamples.ctcf ].map(x => data.zScores.get(x) || -10))
+    }, [ data, biosamples ]);
+
+    return (
+        <div style={{ border: "1px solid", padding: "0.75em", background: "#ffffff" }}>
+            { loading || !data?.accession ? <Loader active /> : (
+                <>
+                    <svg height={18}>
+                        <rect width={10} height={10} y={3} fill={COLORS.get(data.group || "") || "#06da93" } />
+                        <text x={16} y={12}>{props.name} ⸱ {GROUPS.get(data.group || "")}</text>
+                    </svg>
+                    Click for details about this cCRE<br /><br />
+                    <strong>Max Z-scores in brain:</strong><br />
+                    { maxZ && [ "DNase", "H3K4me3", "H3K27ac", "CTCF" ].map( (x, i) => (
+                        <React.Fragment key={i}>
+                            <strong>{x}</strong>: {maxZ && maxZ[x]?.toFixed(2)}<br />
+                        </React.Fragment>
+                    ))}
+                </>
+            )}
+        </div>
+    );
+
+};
+export default CCRETooltip;
---
+++ src/web/cCREDetails/hooks.ts
@@ -0,0 +1,83 @@
+import { ApolloCache, ApolloClient, gql, InMemoryCache, useQuery } from "@apollo/client";
+import { useMemo } from "react";
+
+export type BiosampleEntry = {
+    dnase?: string | null;
+    h3k4me3?: string | null;
+    h3k27ac?: string | null;
+    ctcf?: string | null;
+    name: string;
+    lifeStage: string;
+    ontology: string;
+    sampleType: string;
+};
+
+const BIOSAMPLE_QUERY = gql`
+query {
+    ccREBiosampleQuery(assembly: "grch38") {
+      biosamples {
+        dnase: experimentAccession(assay: "dnase")
+        h3k4me3: experimentAccession(assay: "h3k4me3")
+        h3k27ac: experimentAccession(assay: "h3k27ac")
+        ctcf: experimentAccession(assay: "ctcf")
+        name
+        lifeStage
+        ontology
+        sampleType
+      }
+    }
+}`;
+
+export type CCREInformation = {
+    rDHS: string;
+    accession: string;
+    group: string;
+    coordinates: {
+        chromosome: string;
+        start: number;
+        end: number;
+    };
+    zScores: {
+        experiment: string;
+        score: number;
+    }[];
+};
+
+const CCRE_QUERY = gql`
+query q($rDHS: [String!]) {
+    cCREQuery(rDHS: $rDHS, assembly: "grch38") {
+      rDHS
+      accession
+      group
+      coordinates {
+        chromosome
+        start
+        end
+      }
+      zScores {
+        experiment
+        score
+      }
+    }
+}`;
+
+export function useBiosamples() {
+    const client = useMemo( () => new ApolloClient({
+        uri: "https://psychscreen.api.wenglab.org/graphql",
+        cache: new InMemoryCache()
+    }), []);
+    return useQuery<{ ccREBiosampleQuery: { biosamples: BiosampleEntry[] } }>(BIOSAMPLE_QUERY, { client });
+};
+
+export function useCCREInformation(rDHSAccession: string) {
+    const client = useMemo( () => new ApolloClient({
+        uri: "https://psychscreen.api.wenglab.org/graphql",
+        cache: new InMemoryCache()
+    }), []);
+    const { data, loading } = useQuery<{ cCREQuery: CCREInformation[] }>(CCRE_QUERY, { variables: { rDHS: rDHSAccession }, client });
+    const r = useMemo( () => data?.cCREQuery[0] && ({
+        ...data?.cCREQuery[0],
+        zScores: new Map<string, number>(data?.cCREQuery[0].zScores.map(x => [ x.experiment, x.score ]) || [])
+    }), [ data ]);
+    return { data: r, loading };
+}
