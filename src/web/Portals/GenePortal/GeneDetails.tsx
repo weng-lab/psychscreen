a7fa197dd1d382eb461446e6326c9acc9493368e
NishiPhalke <nishaphalke@gmail.com>
2023-06-01 10:45:03 -0400

    fix switch to another gene functionality on gene portal

--- src/web/Portals/GenePortal/GeneDetails.tsx
+++ src/web/Portals/GenePortal/GeneDetails.tsx
@@ -37,6 +37,16 @@ const GTEX_GENES_QUERY= gql`
       }
   }
 `  
+
+const GENE_ID_QUERY = `
+query ($assembly: String!,  $name_prefix: [String!]) {
+  gene(assembly: $assembly, name_prefix: $name_prefix) {
+    name
+    id
+  }
+}
+`
+
 const GeneDetails: React.FC = (props) => {
     const { gene } = useParams();
     const { state }: any = useLocation();
@@ -48,6 +58,7 @@ const GeneDetails: React.FC = (props) => {
     let { geneid, chromosome, start, end } = state ? state : { geneid: '', chromosome: '', start: null, end: null };
     const [ partialGeneId, setPartialGeneId ] = useState<string | null>(null);
     const [ trueGeneId, setTrueGeneId ] = useState<string | null>(null);
+    const [ trueGeneName, setTrueGeneName ] = useState<string | null>(null);
     if (trueGeneId) geneid = trueGeneId;
 
     const handleTissueCategory = (
@@ -61,13 +72,32 @@ const GeneDetails: React.FC = (props) => {
         setTabIndex(newTabIndex);
     };
 
+    const onGeneChange = React.useCallback(
+      async (value: any) => {
+          
+          const response = await fetch('https://ga.staging.wenglab.org/graphql', {
+              method: 'POST',
+              body: JSON.stringify({
+                  query: GENE_ID_QUERY,
+                  variables: {
+                    name_prefix: [value],
+                    assembly: "GRCh38"
+                  },
+              }),
+              headers: { 'Content-Type': 'application/json' },
+          });
+          const geneID = (await response.json()).data?.gene;          
+          setTrueGeneName(value)
+          setTrueGeneId(geneID[0].id.split(".")[0])
+      },
+      []
+  );
     const { data } = useQuery<GTExGeneQueryResponse>(GTEX_GENES_QUERY, {		
         variables: {
             gene_id: geneid
         },
         skip: geneid === ''
     });
-
   const grouped = useMemo(
       () =>
           groupBy(
@@ -121,12 +151,12 @@ const GeneDetails: React.FC = (props) => {
                 <Grid item sm={9}>
                     <Typography type="headline" size="large" style={{ marginTop: "-0.6em", marginBottom: "0.2em" }}>
                         <img alt="DNA" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Font_Awesome_5_solid_dna.svg/640px-Font_Awesome_5_solid_dna.svg.png" width="1.7%" />
-                        &nbsp;Gene Details: {trueGeneId || gene}
+                        &nbsp;Gene Details: {trueGeneName || gene}
                     </Typography>
                     <div style={{ display: 'flex', alignItems: 'center', fontWeight: 'bold' }}>
                         <span style={{ marginRight: "10px" }}>Switch to another gene:</span>
                         <TextField variant="standard" onChange={(e) => setPartialGeneId(e.target.value)} />
-                        <Button onClick={() => setTrueGeneId(partialGeneId)} variant="outlined" bvariant="filled" btheme="light">Go</Button>
+                        <Button onClick={() => onGeneChange(partialGeneId)} variant="outlined" bvariant="filled" btheme="light">Go</Button>
                     </div>
                 </Grid>
                 <Grid item sm={1} lg={1.5} />
@@ -148,39 +178,39 @@ const GeneDetails: React.FC = (props) => {
                   <Box sx={{ padding: 2 }}>
                     { tabIndex === 0 ? (
                       <Box>
-                        <GeneOverview gene={gene} />
+                        <GeneOverview gene={trueGeneName || gene} />
                       </Box>
                     ) : tabIndex === 2 ? (
                       <Box>
                         <Browser
-                          name={trueGeneId || gene}
+                          name={trueGeneName?.toUpperCase() || gene}
                           coordinates={{ chromosome, start: +start, end: +end }}
                         />
                       </Box>
                     ) : tabIndex === 3 ? (
                       <Box>
-                        <GeneExpressionPage id={geneid}/>
+                        <GeneExpressionPage id={trueGeneId || geneid}/>
                       </Box>
                     ) : tabIndex === 4 ? (
                       <Box>
-                        <AssociatedxQTL name={gene} coordinates={ {chromosome: chromosome,start: parseInt(start),end: parseInt(end)}
+                        <AssociatedxQTL name={trueGeneName?.toUpperCase() || gene} coordinates={ {chromosome: chromosome,start: parseInt(start),end: parseInt(end)}
                         }/>
                       </Box>
                     ) : tabIndex === 6 ? (
                       <Box>
                         <Typography  type="body"
                                     size="small">
-                              <OpenTarget id={geneid}/>
+                              <OpenTarget id={trueGeneId || geneid}/>
                         </Typography>
                       </Box>
                     ) : tabIndex === 5 ? (
                       <Box>
-                          <SingleCell gene={trueGeneId || gene || "APOE"} />
+                          <SingleCell gene={trueGeneName?.toUpperCase() || gene || "APOE"} />
                       </Box>
                     ) : tabIndex === 1 ? (
                       <Box>
                         {(data &&  data?.gtex_genes.length === 0 )  ? <Typography type="body" size="large">
-                          No GTex data found for {gene}
+                          No GTex data found for {trueGeneName?.toUpperCase() || gene}
                         </Typography> : (
                           <>
                                 <Typography type="body" size="large">Group By: </Typography>
