2cfdec8a23540a16a75267a074bb49f476bcf6a7
jpfisher72 <jpfisher72@gmail.com>
2024-06-03 16:28:55 -0400

    Add comma to gene suggestions

--- src/web/Portals/GenePortal/GeneAutocomplete.tsx
+++ src/web/Portals/GenePortal/GeneAutocomplete.tsx
@@ -163,7 +163,7 @@ export const GeneAutoComplete = (props) => {
             }}
             noOptionsText="No Genes Found"
             renderInput={(params) => (
-              <TextField {...params} label={<>e.g. <i>SOX4</i>,<i>GAPDH</i></>} fullWidth />
+              <TextField {...params} label={<>e.g. <i>SOX4</i>, <i>GAPDH</i></>} fullWidth />
             )}
             renderOption={(props, option) => {
               return (
